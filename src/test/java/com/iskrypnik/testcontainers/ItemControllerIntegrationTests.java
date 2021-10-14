276aba874cc50e26cd3feea08f3d314229aa3318
iskrypnik <ilyaskrypnik@gmail.com>
2021-10-14 14:14:25 +0700

    Add integration test with postgres test container

---
+++ src/test/java/com/iskrypnik/testcontainers/ItemControllerIntegrationTests.java
@@ -0,0 +1,35 @@
+package com.iskrypnik.testcontainers;
+
+import com.iskrypnik.testcontainers.model.Item;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@AutoConfigureMockMvc
+class ItemControllerIntegrationTests extends BaseIT {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getItemById() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/item/1"))
+                .andReturn();
+        String jsonResponse = result.getResponse().getContentAsString();
+
+        Item expectedItem = new Item();
+        expectedItem.setId(1);
+        expectedItem.setDescription("this is first item");
+
+        Item receivedItem = objectMapper.readValue(jsonResponse, Item.class);
+        Assertions.assertEquals(expectedItem, receivedItem);
+    }
+}
